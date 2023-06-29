bb0776d2cecca83ded5759e4c59867a401b0cf25
FuckBound <59684700+FuckBound@users.noreply.github.com>
2023-06-30 00:33:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ if __name__ == '__main__':
         bbsid = re.findall(r'oi=[0-9]+', token)[0].replace('oi=', '')
         wait_time = random.randint(1, 3600) # Random Sleep to Avoid Ban
         print(f'为了避免同一时间签到人数太多导致被官方怀疑，开始休眠 {wait_time} 秒')
-        # time.sleep(wait_time)
+        time.sleep(wait_time)
         wallet = r.get(WalletURL, headers=headers, timeout=60)
         if json.loads(wallet.text) == {"data": None,"message":"登录已失效，请重新登录","retcode":-100}: 
             print(f'当前登录已过期，请重新登陆！返回为：{wallet.text}')
